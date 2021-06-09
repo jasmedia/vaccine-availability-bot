90607f90c65a3bac44503ff7a543d7708dd474ae
Jaseem <jaseem@perfotricks.com>
2021-06-09 22:06:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+const env = require('dotenv').config()
 const got = require('got')
+const async = require('async');
 var CronJob = require('cron').CronJob;
 const { format, add } = require('date-fns')
 const logger = require('./config/logger')
@@ -7,23 +8,21 @@ const baseUrl = process.env.BASE_URL;
 const telgramToken = process.env.TELEGRAM_TOKEN;
 const telegramBaseUrl = process.env.TELEGRAM_BASE_URL;
 const channelId = process.env.CHANNEL_ID;
-const districtId = process.env.DISTRICT_ID;
-const centers = [579485, 712743, 628748, 707284, 126367, 126622]
-
-logger.info(`Server initiated on Districst ${districtId} and Centers ${centers}`);
-
+const districtIds = JSON.parse(process.env.DISTRICT_IDS);
+const centers = JSON.parse(process.env.CENTERS);
+logger.info(`Server initiated on Districst ${districtIds} and Centers ${centers}`);
 
 /**
  * Send Message to Telegram
  * @param {String} msg formatted message string
  */
-const sendMessage = async(msg) => {
+const sendMessage = async(name, msg) => {
     // https://api.telegram.org/bot<token>/sendMessage?chat_id=@localcowinavailability&text=Hi
 
     try {
         const url = telegramBaseUrl + `${telgramToken}/sendMessage?chat_id=${channelId}&text=${encodeURI(msg)}&parse_mode=Markdown`
         await got(url);
-        logger.info(`message sent`)
+        logger.info(`message sent ${name}`)
     } catch (error) {
         logger.error(error);
     }
@@ -34,7 +33,7 @@ const sendMessage = async(msg) => {
  * Fetch Vaccine Availability details from Cowin public api.
  * @returns Vaccine availability details
  */
-const fetchAvailabiltyDetails = async() => {
+const fetchAvailabiltyDetails = async(districtId) => {
     return new Promise(async(resolve, reject) => {
         try {
             const date = format(add(new Date(), { days: 0 }), 'dd-MM-yyyy')
@@ -73,40 +72,49 @@ formatMessage = (data) => {
 
 
 const main = () => {
-    fetchAvailabiltyDetails().then(data => {
-        // Send message to Telegram if vaccine slot available.
-        let selectedCenters = data.filter(center => centers.indexOf(center.center_id) !== -1)
+    async.eachSeries(districtIds, function(districtId, callback) {
+        fetchAvailabiltyDetails(districtId).then(data => {
+            // Send message to Telegram if vaccine slot available.
+            let selectedCenters = data.filter(center => centers.indexOf(center.center_id) !== -1)
 
-        logger.info(`Selected Centers: ${selectedCenters.length}`);
-        // Filter vaccine available centers and send message if avaialble.
+            logger.info(`Selected Centers: ${selectedCenters.length}`);
+            // Filter vaccine available centers and send message if avaialble.
+            async.eachSeries(selectedCenters, function(center, cb) {
+                const name = center.name;
+                const address = center.address;
+                const type = center.fee_type;
+                const sessions = center.sessions;
+                sessions.forEach(async session => {
+                    if (session.available_capacity > 0) {
+                        const sessionDate = session.date;
+                        const availability = session.available_capacity;
+                        const ageLimit = session.min_age_limit;
+                        const vaccine = session.vaccine;
+                        const dose1_availability = session.available_capacity_dose1;
+                        const dose2_availability = session.available_capacity_dose2;
+                        const formatted = await formatMessage({ name, address, type, sessionDate, availability, ageLimit, vaccine, dose1_availability, dose2_availability })
 
-        selectedCenters.forEach(center => {
-            const name = center.name;
-            const address = center.address;
-            const type = center.fee_type;
-            const sessions = center.sessions;
-            sessions.forEach(async session => {
-                if (session.available_capacity > 0) {
-                    const sessionDate = session.date;
-                    const availability = session.available_capacity;
-                    const ageLimit = session.min_age_limit;
-                    const vaccine = session.vaccine;
-                    const dose1_availability = session.available_capacity_dose1;
-                    const dose2_availability = session.available_capacity_dose2;
-                    const formatted = await formatMessage({ name, address, type, sessionDate, availability, ageLimit, vaccine, dose1_availability, dose2_availability })
+                        await sendMessage(name, formatted)
+                    } else {
+                        logger.warn(`Not available at: ${name}`);
+                    }
+                })
+                cb();
+            }, function(err) {
+                if (err) logger.error(err);
+                callback();
 
-                    await sendMessage(formatted)
-                } else {
-                    logger.warn(`Not available at: ${name}`);
-                }
             })
-        })
 
 
 
-    }).catch(err => {
-        logger.error(err);
-    });
+        }).catch(err => {
+            logger.error(err);
+            callback();
+        });
+    }, function(err) {
+        console.log('Finished');
+    })
 
 }
 
